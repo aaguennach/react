2444aa8a2e97b36da741e18dccdabd5e603a31ab
aaguennach <127617154+aaguennach@users.noreply.github.com>
2024-10-25 15:44:38 +0100

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
 
 function App() {
+  const nbArt = 6;
+  let remise = 0;
+  if (nbArt >= 5) {
+    remise = 2;
+  }
   return (
-    <>
-    </>
+    <div>
+      <h3> Remise </h3>
+      <p> Remise est: {remise} %</p>
+    </div>
   );
 }
 
